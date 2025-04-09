a075c3b644c7b8698d83e7706a3da47537298313
Garvit <taneja.garvit29@gmail.com>
2025-04-09 16:00:02 +0530

    updated db

--- db/db.go
+++ db/db.go
@@ -1,6 +1,26 @@
 package db
 
-import "go.mongodb.org/mongo-driver/mongo"
+import (
+	"context"
+	"log"
 
-var Client *mongo.Client
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
 
+var Client *mongo.Client             // Global MongoDB client (like a db connection pool)
+var UserCollection *mongo.Collection // Global reference to "users" collection
+
+func InitDB() error {
+	clientOptions := options.Client().ApplyURI("Mongo connection string")
+
+	client, err := mongo.Connect(context.Background(), clientOptions)
+	if err != nil {
+		return err
+		}
+		Client = client
+		UserCollection = client.Database("authdb").Collection("users")
+		log.Println("Connected to MongoDB")
+		return nil
+
+}
