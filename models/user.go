785b1f0269642ff6c5958b3384bdab8ff78fa55f
Garvit <taneja.garvit29@gmail.com>
2025-04-10 12:17:57 +0530

    update models

--- models/user.go
+++ models/user.go
@@ -1,7 +1,7 @@
 package models
 
 type User struct{
-	ID string `json:"id" bson:"_id", omitempty` // omitempty is used to make the field optional
+	ID       string `json:"id" bson:"_id,omitempty"` // omitempty is used to make the field optional
 	Email string `json:"email" bson:"email" validate:"required,email"` // validate is used to
 	Password string `json:"password" bson:"password"`
 }
